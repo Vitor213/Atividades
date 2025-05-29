62a5a2d0aaf215450e29d72137ef90f94bddc104
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2025-05-28 22:04:00 -0300

    iiiffff

---
+++ EX005/condition.js
@@ -0,0 +1,8 @@
+var velocidade = 50.5;
+var limiteVia = 60;
+
+if (velocidade > limiteVia) {
+    console.log("Você foi multado por excesso de velocidade!");
+} else {
+    console.log("Mantenha a velocidade da via.");
+}
---
+++ EX006/condition2.js
@@ -0,0 +1,9 @@
+var nacionalidade = 'brasileiro';
+
+console.log('Você é um cidadão ' + nacionalidade)
+
+if (nacionalidade == 'brasileiro') {
+    console.log('Brasileiro');
+}else {
+    console.log('estrangeiro')
+}
