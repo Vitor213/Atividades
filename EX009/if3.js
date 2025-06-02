3ca5e9bd4c962c681bfb0d461b48190de82bb3e8
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2025-06-02 20:39:40 -0300

    teste de condições

---
+++ EX009/if3.js
@@ -0,0 +1,9 @@
+var idade = 20
+if (idade < 16){
+    console.log('Você não vota amigo. ')
+} else{ 
+    if (idade >= 16 && idade < 18) 
+    console.log('Você pode votar, mas não é obrigado. ')
+}   if (idade >=18){
+    console.log('Voto obrigatorio. ')
+}
