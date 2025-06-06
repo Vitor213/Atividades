fd059738c6d42bfd5417f70812e938ed72eb0e95
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2025-06-05 22:19:08 -0300

    rlx to podre hj

--- Projetos--Metade/horas-site-js/script.js
+++ Projetos--Metade/horas-site-js/script.js
@@ -2,10 +2,20 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora =
+var hora = data.getHours
 msg.innerHTML = 'agora são '+ hora+ ' horas.'
-if (hora >= 12 && hora <= 18){
+if (hora >= 0 && hora < 12){
+    // Bom dia
+    img.src = 'foto-manha.png'
+    document.body.style.background = '#afb0aa'
+} else if (hora >= 12 && hora <= 18){
     // Boa tarde
+    img.src = 'foto-manha.png'
+    document.body.style.background = '#afb7aa'
+} else{
+    //Boa noite
+    img.src = 'foto-manha.png'
+    document.body.style.background = '#afb7ba'
 }
 
 }
