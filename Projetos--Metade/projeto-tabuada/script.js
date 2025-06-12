10547398c1770689bbd1bf7582d98c999bcdfff1
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2025-06-11 22:57:28 -0300

    I'm going to use "while" to finish this

---
+++ Projetos--Metade/projeto-tabuada/script.js
@@ -0,0 +1,10 @@
+function calcular(){
+    let num = document.getElementById('tabu')
+    let tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+    }
+}
