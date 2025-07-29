a88494d9efe3b79051371e6e890d21dd67e74123
Vitor Teixeira <vitormoises838@gmail.com>
2025-07-29 16:55:57 -0300

    Add finalize functionality and fix result display
    Added the 'finalizar' function to compute and display statistics (total, max, min, sum, average) for the entered numbers. Fixed the result element reference and connected the 'Finalizar' button to the new function. Also improved the add operation to update the list and clear the result area.

--- Projetos--Metade/recursividade/js/script.js
+++ Projetos--Metade/recursividade/js/script.js
@@ -1,6 +1,6 @@
 let num = document.getElementById('fnumero')
 let lista = document.getElementById('flist')
-let resultado = document.getElementById('res')
+let res = document.getElementById('resultado')
 let valores = []
 
 function isnumero(n){
@@ -21,8 +21,44 @@ function inlist(n, l){
 
 function adicionar(){
     if(isnumero(num.value) && !inlist(num.value, valores)){
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adiconado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor errado ou já encontrado na lista!')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar (){
+    if(valores.length == 0){
+        window.alert('Nao a dados para finalizar o processo')
+    } else{
+
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }   
+        media = soma / total
+        
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${total} elementos no total. </p>`
+        res.innerHTML += `<p> O maior valor e ${maior}. </p>`
+        res.innerHTML += `<p> O menor valor e ${menor}. </p>`
+        res.innerHTML += `<p> A soma desses valores e de ${soma}. </p>`
+        res.innerHTML += `<p> E sua media de ${media}. </p>`
     }
 }
